621310a7955fe803b1ac0cf33b74149e9089ba19
Robodav <dvandiver64@gmail.com>
2020-05-14 16:09:35 -0600

    Create catnapping.py

--- ch5/inventory.py
+++ ch5/inventory.py
@@ -15,24 +15,23 @@ def addToInventory(inventory, addedItems):
         return
     for item in addedItems:
         inventory.setdefault(item, 0)
-    for item in addedItems:
         inventory[item] += 1
 
 inventory1 = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 inventory2 = 'hello!'
 
-displayInventory(inventory1)
+# displayInventory(inventory1)
 # displayInventory(inventory2)
 
 adding1 = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 adding2 = ['gold coin', 'dagger', 'dagger', 'bow', 'potion']
 adding3 = 'hello!'
 
-addToInventory(inventory1, adding1)
-displayInventory(inventory1)
+# addToInventory(inventory1, adding1)
+# displayInventory(inventory1)
 
-addToInventory(inventory1, adding2)
-displayInventory(inventory1)
+# addToInventory(inventory1, adding2)
+# displayInventory(inventory1)
 
-addToInventory(inventory1, adding3)
-displayInventory(inventory1)    
+# addToInventory(inventory1, adding3)
+# displayInventory(inventory1)    
