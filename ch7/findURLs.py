d7d45be0687cdd6302f880328b7ae21379453331
Vandiver <DVandive@jbssa.com>
2020-07-06 12:18:59 -0600

    Create URL copier

--- ch7/findURLs.py
+++ ch7/findURLs.py
@@ -10,3 +10,16 @@ urlRegex = re.compile(r'''(
     (\.[a-zA-Z]{2,4})       # dot something
     (\.[a-zA-Z]{2,4})?      # optional dot something
 )''', re.VERBOSE)
+
+text = str(pyperclip.paste())
+
+matches = []
+for groups in urlRegex.findall(text):
+    matches.append(groups[0])
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No URLs found.')
