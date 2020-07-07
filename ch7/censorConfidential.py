08046d55c8b9877ef705ea6e7e127ad9dcdd86ac
Vandiver <DVandive@jbssa.com>
2020-07-07 10:26:12 -0600

    Complete censorConfidential.py

--- ch7/censorConfidential.py
+++ ch7/censorConfidential.py
@@ -8,10 +8,11 @@ socialRegex = re.compile(r'''(
     (\d{3})    # First 3 numbers
     (\s|-|/)   # Separator
     (\d{2})    # Middle 2 numbers
-    (\s|-|/)   # Separator
+    (\s|-|/)  # Separator
     (\d{4})    # Last 4 numbers
 )''', re.VERBOSE)
 
+# Finds credit card numbers
 cardRegex = re.compile(r'''(
     (\d{4})     # First 4 numbers
     (\s|-|/)?   # Separator
@@ -22,6 +23,10 @@ cardRegex = re.compile(r'''(
     (\d{4})     # Last 4 numbers
 )''', re.VERBOSE)
 
-#TODO: Censor copied text
+text = str(pyperclip.paste())
 
-#TODO: Paste censored text to clipboard
+newText = socialRegex.sub('***-**-****', text)
+newerText = cardRegex.sub('****-****-****-****', newText)
+
+pyperclip.copy(newerText)
+print('Copied to clipboard: ' + newerText)
