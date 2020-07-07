8e713195b1cdd625e20e8050ab2d7f6e38a9f94a
Vandiver <DVandive@jbssa.com>
2020-07-07 12:32:00 -0600

    Fix dateRegex and begin finding matches

--- ch7/dateDetection.py
+++ ch7/dateDetection.py
@@ -3,12 +3,17 @@
 
 import re
 
-dateRegex = (r'''(
-    [0-3][0-9]/    # Day
-    [0-1][0-2]/    # Month
-    [1-2][0-9]{3}    # Year
+dateRegex = re.compile(r'''(
+    ([0-3][0-9])/    # Day
+    ([0-1][0-9])/    # Month
+    ([1-2][0-9]{3})    # Year
 )''', re.VERBOSE)
 
-#TODO: Store month/day/year into variables
+def dateDetection(text):
+    matches = dateRegex.findall(text)
+    print(matches)
 
 #TODO: Detect if valid date
+
+text = "01/01/1999, 02/03/2000, 05/20/1970, 2/4/40, 09/09/0004"
+dateDetection(text)
