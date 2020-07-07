2bf26b7d3a06e8595f6a1193aafd4d9b15bcda57
Vandiver <DVandive@jbssa.com>
2020-07-07 12:51:20 -0600

    Make checkDate

--- ch7/dateDetection.py
+++ ch7/dateDetection.py
@@ -11,9 +11,34 @@ dateRegex = re.compile(r'''(
 
 def dateDetection(text):
     matches = dateRegex.findall(text)
-    print(matches)
+    for match in matches:
+        if checkDate(match):
+            print("VALID: " + match[0])
+        else:
+            print("INVALID: " + match[0])
 
-#TODO: Detect if valid date
+def checkDate(date):
+    try:
+        day = int(date[1])
+        month = int(date[2])
+        year = int(date[3])
+    except:
+        print('Please enter a valid date')
+        return False
+    if month in [4,6,9,11]:
+        if day > 30:
+            return False
+    elif month == 2:
+        if day > 28:
+            if day == 29: # February 29 only on leap years
+                if not (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                    return False
+            else:
+                return False
+    else:
+        if day > 31:
+            return False
+    return True
 
 text = "01/01/1999, 02/03/2000, 05/20/1970, 2/4/40, 09/09/0004"
 dateDetection(text)
