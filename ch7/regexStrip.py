eaedc57b93b56f69206239d59e302d86048f6b99
Vandiver <DVandive@jbssa.com>
2020-07-08 10:58:43 -0600

    Complete regexStrip.py

--- ch7/regexStrip.py
+++ ch7/regexStrip.py
@@ -1,5 +1,17 @@
 import re
 
-#TODO: Write function to isolate regex characters
 def regexStrip(text, removing=' '):
-    removingReg = re.compile(r'[%]')
+    """Same function as python's .strip() method, uses regex"""
+    removingReg = re.compile(rf'{removing}*(.*?){removing}*$')
+    matched = removingReg.search(text)
+    return matched.group(1)
+
+# test1 = "             boneless              "
+# test2 = "           bone              less            "
+# test3 = "0000000000bone00000000000000less00000000000000"
+# test4 = "boneless"
+
+# print(regexStrip(test1))
+# print(regexStrip(test2))
+# print(regexStrip(test3, '0'))
+# print(regexStrip(test4))
