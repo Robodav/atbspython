5ed99cd8cf442949d2ab073ea9a77ec0a42461e1
Vandiver <DVandive@jbssa.com>
2020-07-09 13:53:23 -0600

    Add condiment choices

--- ch8/sandwichMaker.py
+++ ch8/sandwichMaker.py
@@ -12,3 +12,10 @@ wantCheese = pyip.inputYesNo(prompt='Would you like cheese?\n')
 if wantCheese:
     cheesechoices = ['cheddar', 'swiss', 'mozarella']
     cheese = pyip.inputMenu(cheesechoices, numbered=True)
+
+wantMayo = pyip.inputYesNo(prompt='Do you want mayo?\n')
+wantMustard = pyip.inputYesNo(prompt='Mustard?\n')
+wantLettuce = pyip.inputYesNo(prompt='Lettuce?\n')
+wantTomato = pyip.inputYesNo(prompt='Tomato?\n')
+
+numSandwiches = 
