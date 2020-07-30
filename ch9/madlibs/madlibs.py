d2683f880da446fab1624cc507a19fb828f7a3cf
Vandiver <DVandive@jbssa.com>
2020-07-30 13:18:56 -0600

    Add mad libs text file selection and reading

--- ch9/madlibs/madlibs.py
+++ ch9/madlibs/madlibs.py
@@ -1,3 +1,14 @@
 #! python3
 # madlibs.py - madlibs game program using user input and text files
 
+from pathlib import Path
+import pyinputplus as pyip
+
+#Read mad libs text file
+selection = pyip.inputMenu(['1', '2', '3']
+madlibFile = open(Path.cwd() / f'madlibs{selection}.txt')
+madlibContent = madlibFile.read()
+
+#TODO: Find replacement occurrences and prompt user to write over them
+
+#TODO: Print results to screen and save to new text file
