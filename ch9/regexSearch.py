8076cf5390c61bd37b6851f92e0e76a47275a3ff
Robodav <dvandiver64@gmail.com>
2020-07-30 20:30:22 -0600

    Layout specifications

---
+++ ch9/regexSearch.py
@@ -0,0 +1,9 @@
+#! python3
+# regexSearch.py - Opens all .txt files in a folder and searches for any line
+#                  that matches a user-specified regex
+
+#TODO: Open a specified directory
+
+#TODO: Get a regex input from the user
+
+#TODO: Find all matches in all text documents and print to the screen
