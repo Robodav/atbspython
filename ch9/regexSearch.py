db769cde6beea10bf6352482d12c85e969065f24
Robodav <dvandiver64@gmail.com>
2020-07-30 20:55:01 -0600

    Find all matches in all text documents and print to the screen

--- ch9/regexSearch.py
+++ ch9/regexSearch.py
@@ -12,4 +12,18 @@ textFiles = list(directory.glob('*.txt'))
 # Get a regex input from the user
 reg = pyip.inputRegexStr(prompt='Enter a regex to match:\n')
 
-#TODO: Find all matches in all text documents and print to the screen
+# Find all matches in all text documents and print to the screen
+for t in textFiles:
+    pattern = re.compile(reg)
+
+    searching = open(t)
+    searchingContent = searching.read()
+    searching.close()
+
+    matches = pattern.findall(searchingContent)
+    if len(matches) == 0:
+        print(f'No matches found in {t}.')
+    else:
+        print(f'Matches found in {t}:')
+        for match in matches:
+            print(match)
