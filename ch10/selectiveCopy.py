8673e9ee6fa579bbbb85185e3eaf351bb3272411
Robodav <dvandiver64@gmail.com>
2020-08-16 17:44:24 -0600

    Copy files to copyTo folder

--- ch10/selectiveCopy.py
+++ ch10/selectiveCopy.py
@@ -9,15 +9,20 @@ def selectiveCopy():
     # Walk through copyFrom folder tree and store paths
     copyFrom = pyip.inputFilepath(prompt='Enter the path of the folder to copy from:\n', mustExist=True)
     extension = pyip.inputRegex('[a-zA-Z]', prompt='Enter a file extension:\n')
+    copyTo = Path(pyip.inputFilepath(prompt='Enter the path of the folder to copy to:\n'))
+    if not Path.is_dir(copyTo):
+        os.makedirs(copyTo) # make the destination directory
     filePaths = []
     for root, dirs, files in os.walk(copyFrom):
         print(f'Checking {root}...')
         for f in files:
             if f.endswith(extension):
                 filePath = Path(root) / f
-                print(f'Copying {filePath}...')
                 filePaths.append(filePath)
 
-    # TODO: Copy files at stored paths to copyTo folder
+    # Copy files at stored paths to copyTo folder
+    for filePath in filePaths:
+        print(f'Copying {filePath}...')
+        shutil.copy(filePath, copyTo)
 
 selectiveCopy()
