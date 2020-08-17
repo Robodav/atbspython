4d394d0a43aa0038496699ff742211bb4c3fed90
Vandiver <DVandive@jbssa.com>
2020-08-17 13:03:49 -0600

    Change from function to list value

--- ch10/fillTheGaps.py
+++ ch10/fillTheGaps.py
@@ -12,26 +12,28 @@ organizing = pyip.inputFilepath(prompt='Enter the folder to search:\n', mustExis
 prefix = pyip.inputStr(prompt='Enter the prefix of each file:\n')
 prefixregex = re.compile(rf'({prefix}[0]*)(\d+)(.*)')
 
-# Loop through the directory, saving numbers and extensions to a dictionary
+# Loop through the directory, saving information to a dictionary
 files = os.listdir(organizing)
+fileData = {}
 for f in files:
     matched = prefixregex.match(f)
     if matched is not None:
-        fillGaps(matched.group(1), matched.group(2), matched.group(3))
+        fileData[matched.group(2)] = [matched.group(1), matched.group(3)]
 
-def fillGaps(prefix, num, extension):
-    # Strip and sort all numbers matched, saving them to a list
-    fileNums = list(fileData.keys())
-    for i in range(len(fileNums)):
-        fileNums[i] = int(fileNums[i].lstrip('0'))
-    fileNums.sort()
+# Strip and sort all numbers matched, saving them to a list
+fileNums = list(fileData.keys())
+for i in range(len(fileNums)):
+    fileNums[i] = int(fileNums[i].lstrip('0'))
+fileNums.sort()
 
-    # Loop through saved numbers and find gaps, renaming associated files
-    for i in range(len(fileNums)-1):
-        if not fileNums[i+1] == fileNums[i] + 1:
-            rootPath = Path(organizing)
-            extension = fileData[str(fileNums[i+1])]
-            oldPath = rootPath / (prefix + str(fileNums[i+1]) + extension)
-            newPath = rootPath / (prefix + str(fileNums[i] + 1) + extension)
-            print(f'Renaming {oldPath} to {newPath}')
-            # shutil.move(oldPath, newPath) # uncomment after testing
+# Loop through saved numbers and find gaps, renaming associated files
+for i in range(len(fileNums)-1):
+    if not fileNums[i+1] == fileNums[i] + 1:
+        renaming = fileNums[i+1]
+        rootPath = Path(organizing)
+        prefix = fileData[str(renaming)][0]
+        extension = fileData[str(renaming)][1]
+        oldPath = rootPath / f'{prefix}{renaming}{extension}'
+        newPath = rootPath / f'{prefix}{fileNums[i] + 1}{extension}'
+        print(f'Renaming {oldPath} to {newPath}')
+        shutil.move(oldPath, newPath) # uncomment after testing
