afc6d2fbe4bb76169b0a2cde470096f961ac4c99
Robodav <dvandiver64@gmail.com>
2020-12-17 19:58:43 -0700

    Save image and get prev button's url

--- ch12/downloadXkcd.py
+++ ch12/downloadXkcd.py
@@ -14,7 +14,7 @@ while not url.endswith('#'):
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     # Find the URL of the comic image.
-    comicElem = soup.select('#comic img')
+    comicElem = soup.select('#comic > a > img')
     if comicElem == []:
         print('Could not find comic image.')
     else:
@@ -24,8 +24,13 @@ while not url.endswith('#'):
         res = requests.get(comicUrl)
         res.raise_for_status()
 
-    # TODO: Save the image to ..\..\xkcd.
+    # Save the image to ..\..\xkcd.
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
 
-    # TODO: Get the Prev button's url.
+    # Get the Prev button's url.
+    prevLink = soup.select('a[rel="prev"]')[0]
 
 print('Done.')
