9d81f25234ece6c7d441cd2f60bb5add9130316e
Robodav <dvandiver64@gmail.com>
2020-12-17 19:32:20 -0700

    Retrieve top results and open a browser tab for each

--- ch12/searchpypi.py
+++ ch12/searchpypi.py
@@ -7,6 +7,13 @@ res = requests.get('https://google.com/search?=' 'https://pypi.org/search/?q='
  + ''.join(sys.argv[1:]))
  res.raise_for_status()
 
- # TODO: Retrieve top search result links.
+ # Retrieve top search result links.
+ soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
- # TODO: Open a browser tab for each result.
+ # Open a browser tab for each result.
+ linkElems = soup.select('.package=snippet')
+ numOpen = min(5, len(linkElems))
+ for i in range(numOpen):
+     urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+     print('Opening', urlToOpen)
+     webbrowser.open(urlToOpen)
